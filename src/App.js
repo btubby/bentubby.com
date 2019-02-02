4ace515fe35a9de2c2def838997da1bbab479627
Ben Tubby <btubby@gmail.com>
2019-02-02 14:38:15 +0000

    initial work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import tubby from "./Assets/ThorbV3.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <div id="container">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <a href="https://en.wikipedia.org/wiki/Red">Ben Tubby</a>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+          {/* <p>
+            <a href="https://en.wikipedia.org/wiki/Blue">Home</a>
+          </p> */}
+
+          <p>
+            <a href="https://en.wikipedia.org/wiki/Yellow">About</a>
+          </p>
+
+          <p>
+            <a href="https://en.wikipedia.org/wiki/Green">Work</a>
+          </p>
+
+          <p>
+            <a href="https://en.wikipedia.org/wiki/Orange_(colour)">Contact</a>
+          </p>
+
+          <p>
+            <a href="https://www.flickr.com/photos/tubby/">Photography</a>
+          </p>
+        </div>
+
+        <div className="App">
+          <header className="App-header">
+            <img src={tubby} className="App-logo" alt="logo" />
+          </header>
+        </div>
       </div>
     );
   }
