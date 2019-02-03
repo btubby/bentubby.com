d0352298eeba56813031f698d38b10be89d5497c
Ben Tubby <btubby@gmail.com>
2019-02-03 15:50:18 +0000

    formatting

--- src/App.js
+++ src/App.js
@@ -5,11 +5,22 @@ import "./App.css";
 import styled from "styled-components";
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 90%;
   margin: auto;
   height: 100vh;
   padding-top: 4%;
 `;
+const Inner = styled.div`
+display: flex;
+flex-direction: column;
+  @media (max-height: 450px) {
+   flex-direction:row
+    justify-content:space-between;
+  }
+`;
+
 const Section = styled.div`
   width: 80%;
   margin: auto;
@@ -22,37 +33,66 @@ const About = styled.div`
   color: #fff;
   font-family: Iceland;
   line-height: 2em;
+  @media (max-height: 450px) {
+    font-size: 2em;
+  }
+  @media (max-width: 650px) {
+    line-height: 1em;
+  }
 `;
+
 class App extends Component {
+  componentDidMount() {
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = () => {
+      this.scrollFunction();
+    };
+  }
+
+  scrollFunction = () => {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      document.getElementById("myBtn").style.display = "block";
+    } else {
+      document.getElementById("myBtn").style.display = "none";
+    }
+  };
+  // When the user clicks on the button, scroll to the top of the document
+  topFunction = () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  };
+
   render() {
     return (
       <div>
+        <div class="item name">Ben Tubby</div>
+        <button onClick={this.topFunction} id="myBtn" title="Go to top">
+          Top
+        </button>
         <Container>
-          <p>Ben Tubby</p>
-
-          {/* <p>
-            <a href="https://en.wikipedia.org/wiki/Blue">Home</a>
-          </p> */}
-
-          <div class="item about">
-            <a href="#about">About</a>
-          </div>
-
-          <p>
-            <a target="_" href="http://www.fairfx.com">
-              Work
-            </a>
-          </p>
-
-          <p>
-            <a href="#contact">Contact</a>
-          </p>
-
-          <p>
-            <a target="_" href="https://www.flickr.com/photos/tubby/">
-              Photography
-            </a>
-          </p>
+          <Inner>
+            <div class="item about">
+              <a href="#about">About</a>
+            </div>
+            <div class="item photography">
+              <a target="_" href="https://www.flickr.com/photos/tubby/">
+                Photography
+              </a>
+            </div>
+          </Inner>
+          <Inner>
+            <div class="item work">
+              <a target="_" href="http://www.fairfx.com">
+                Work
+              </a>
+            </div>
+            <div class="item contact">
+              <a href="#contact">Contact</a>
+            </div>
+          </Inner>
         </Container>
 
         <div className="App">
@@ -60,10 +100,10 @@ class App extends Component {
             <img src={tubby} className="App-logo" alt="logo" />
           </header>
         </div>
+
         <Section>
           <a name="about" />
           <About>
-            <br />
             Hi, I'm an experienced full stack developer based in the UK.
             <br />I enjoy&nbsp;
             <span class="about">
@@ -82,7 +122,6 @@ class App extends Component {
               .
             </span>
             <br />
-            <br />
             Find me on&nbsp;
             <span class="about">
               <a target="_" href="https://www.linkedin.com/in/bentubby/">
@@ -92,6 +131,7 @@ class App extends Component {
             </span>
           </About>
         </Section>
+
         <Section>
           <About>
             <a name="contact" />
